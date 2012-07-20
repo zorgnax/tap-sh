1bad5f09b2a37ca38e6da05b102cd9ee73fecc9b
jgelbman <gelbman@gmail.com>
2012-07-20 18:15:08 -0500

    Check if value is integer in val_nok

--- tap.sh
+++ tap.sh
@@ -85,6 +85,9 @@ val_ok () {
 
 val_nok () {
     local value=$1 desc=$2
+    if ! [[ "$value" =~ ^[0-9]+$ ]]; then
+        value=1
+    fi
     if [ "$value" -eq 0 ]; then
         fail "$desc"
     else
