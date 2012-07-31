1b49b2da86330c9406c3763f49bb183bf97af82b
jgelbman <gelbman@gmail.com>
2012-07-30 23:26:16 -0500

    Fill desc variable only if not provided

--- t/test.sh
+++ t/test.sh
@@ -1,11 +1,13 @@
 . tap.sh
 
 tap_is () {
-    local cmd=$1 expected=$2 desc
-    if [[ "$cmd" =~ $'\n' ]]; then
-        desc=$(head -1 <<< "$cmd")...
-    else
-        desc=$cmd
+    local cmd=$1 expected=$2 desc=$3
+    if [ -z "$desc" ]; then
+        if [[ "$cmd" =~ $'\n' ]]; then
+            desc=$(head -1 <<< "$cmd")...
+        else
+            desc=$cmd
+        fi
     fi
     cmd=". tap.sh;$cmd"
     sub_run_is "$cmd" "$expected" "$desc"
