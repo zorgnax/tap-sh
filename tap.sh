c5bc04e2a672d390e76b80c42b1b0053c99b69c3
jgelbman <gelbman@gmail.com>
2012-07-31 00:10:05 -0500

    Move tap_is into main library

--- tap.sh
+++ tap.sh
@@ -335,3 +335,16 @@ sub_run_unlike () {
     unlike "$GOT" "$regex" "$desc"
 }
 
+tap_is () {
+    local cmd=$1 expected=$2 desc=$3
+    if [ -z "$desc" ]; then
+        if [[ "$cmd" =~ $'\n' ]]; then
+            desc=$(head -1 <<< "$cmd")...
+        else
+            desc=$cmd
+        fi
+    fi
+    cmd=". tap.sh;$cmd"
+    sub_run_is "$cmd" "$expected" "$desc"
+}
+
